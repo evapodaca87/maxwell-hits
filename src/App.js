90eb0c2597f165eda7612dd998dc972b1a7ac052
evapodaca87 <evapodaca87@gmail.com>
2018-12-18 18:47:56 -0700

    added home page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ class App extends Component {
     render() {
         return (
             <div className='App'>
-                <p>Maxwell Hits</p>
+                <h1>Maxwell Hits</h1>
+                <div className='nav'>
+                    <h2 className="btn">Music</h2>
+                    <h2 className="btn">Contact</h2>
+                </div>
             </div>
         );
     }
