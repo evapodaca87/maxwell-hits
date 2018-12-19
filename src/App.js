378e5f7fa37e47fcb827993f27b06980f9958739
evapodaca87 <evapodaca87@gmail.com>
2018-12-18 18:54:29 -0700

    finished homme page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ class App extends Component {
     render() {
         return (
             <div className='App'>
-                <h1>Maxwell Hits</h1>
-                <div className='nav'>
-                    <h2 className="btn">Music</h2>
-                    <h2 className="btn">Contact</h2>
+                <div className='container'>
+                    <h1>Maxwell Hits</h1>
+                    <div className='nav'>
+                        <h2 className='btn'>Music</h2>
+                        <h2 className='btn'>Contact</h2>
+                    </div>
                 </div>
             </div>
         );
