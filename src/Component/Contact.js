b9faa24048e2c0cdb9bdf2a2a78550d9ce813711
evapodaca87 <evapodaca87@gmail.com>
2018-12-20 11:32:38 -0700

    fixed flex style on music and contact pages

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Contact = () => (
     <div className='contact'>
+    <div className="space"></div>
         <h2>Contact</h2>
         <div className='foot'>
             <Link to='/'>Home</Link>
--- src/Component/Music.js
+++ src/Component/Music.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Music = () => (
     <div className='music'>
+    <div className="space"></div>
         <h2>Music</h2>
 
         <div className='foot'>
