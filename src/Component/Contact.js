28cf41e1acc0edcdc20c90be1486311188720798
evapodaca87 <evapodaca87@gmail.com>
2018-12-20 11:10:27 -0700

    added button to navigate home

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Contact = () => (
     <div className='contact'>
         <h2>Contact</h2>
+        <div className='foot'>
+            <Link to='/'>Home</Link>
+        </div>
     </div>
 );
 export default Contact;
--- src/Component/Music.js
+++ src/Component/Music.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Music = () => (
     <div className='music'>
         <h2>Music</h2>
+
+        <div className='foot'>
+            <Link to='/'>Home</Link>
+        </div>
     </div>
 );
 export default Music;
