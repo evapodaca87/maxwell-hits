7a11da12f9de0eb59cd02e6a911dfe0030b8df9f
evapodaca87 <evapodaca87@gmail.com>
2018-12-23 20:26:07 -0700

    images

--- src/Component/Music.js
+++ src/Component/Music.js
@@ -3,16 +3,17 @@ import { Link } from 'react-router-dom';
 const {Howl, Howler} = require('howler');
 
 var sound = new Howl({
-    src: ['sound.mp3']
+    src: ['/Music/bonus.mp3']
   });
   
-  sound.play();
+//   sound.play();
 
 const Music = () => (
     <div className='music'>
         <div className='space' />
         <div className='songs'>
             <h2>Music Page</h2>
+            <p>Bonus <img src="/images.randomFern.png"></img></p>
         </div>
 
         <div className='foot'>
