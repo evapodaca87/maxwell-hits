7f92fe81749e01631dfba5303aae992c94303fab
evapodaca87 <evapodaca87@gmail.com>
2018-12-23 21:54:56 -0700

    music template finished and running

--- src/Component/Music.js
+++ src/Component/Music.js
@@ -10,74 +10,182 @@ import noBrakesPic from '../images/noBrakes.png';
 import wcwPic from '../images/wcw.png';
 const { Howl, Howler } = require('howler');
 
-var sound = new Howl({
+var bonusSong = new Howl({
     src: [ '/Music/bonus.mp3' ]
 });
+var byTheWaySong = new Howl({
+    src: [ '/Music/byTheWay.mp3' ]
+});
+var disillusionedSong = new Howl({
+    src: [ '/Music/disillusioned.mp3' ]
+});
+var giveUupSong = new Howl({
+    src: [ '/Music/giveUup.mp3' ]
+});
+var mean2meSong = new Howl({
+    src: [ '/Music/mean2me.mp3' ]
+});
+var mooncruiseSong = new Howl({
+    src: [ '/Music/mooncruise.mp3' ]
+});
+var nobrakesSong = new Howl({
+    src: [ '/Music/nobrakes.mp3' ]
+});
+var wcwSong = new Howl({
+    src: [ '/Music/WCW.mp3' ]
+});
+let bonus = () => {
+    bonusSong.pause();
+    byTheWaySong.stop();
+    disillusionedSong.stop();
+    giveUupSong.stop();
+    mean2meSong.stop();
+    mooncruiseSong.stop();
+    nobrakesSong.stop();
+    wcwSong.stop();
+    bonusSong.play();
+}
+let byTheWay = () => {
+    bonusSong.stop();
+    byTheWaySong.pause();
+    disillusionedSong.stop();
+    giveUupSong.stop();
+    mean2meSong.stop();
+    mooncruiseSong.stop();
+    nobrakesSong.stop();
+    wcwSong.stop();
+    byTheWaySong.play();
+}
+let disillusioned = () => {
+    bonusSong.stop();
+    byTheWaySong.stop();
+    disillusionedSong.pause();
+    giveUupSong.stop();
+    mean2meSong.stop();
+    mooncruiseSong.stop();
+    nobrakesSong.stop();
+    wcwSong.stop();
+    disillusionedSong.play();
+}
+let giveUup = () => {
+    bonusSong.stop();
+    byTheWaySong.stop();
+    disillusionedSong.stop();
+    giveUupSong.pause();
+    mean2meSong.stop();
+    mooncruiseSong.stop();
+    nobrakesSong.stop();
+    wcwSong.stop();
+    giveUupSong.play();
+}
+let mean2me = () => {
+    bonusSong.stop();
+    byTheWaySong.stop();
+    disillusionedSong.stop();
+    giveUupSong.stop();
+    mean2meSong.pause();
+    mooncruiseSong.stop();
+    nobrakesSong.stop();
+    wcwSong.stop();
+    mean2meSong.play();
+}
+let mooncruise = () => {
+    bonusSong.stop();
+    byTheWaySong.stop();
+    disillusionedSong.stop();
+    giveUupSong.stop();
+    mean2meSong.stop();
+    mooncruiseSong.pause();
+    nobrakesSong.stop();
+    wcwSong.stop();
+    mooncruiseSong.play();
+}
+let nobrakes = () => {
+    bonusSong.stop();
+    byTheWaySong.stop();
+    disillusionedSong.stop();
+    giveUupSong.stop();
+    mean2meSong.stop();
+    mooncruiseSong.stop();
+    nobrakesSong.pause();
+    wcwSong.stop();
+    nobrakesSong.play();
+}
+let wcw = () => {
+    bonusSong.stop();
+    byTheWaySong.stop();
+    disillusionedSong.stop();
+    giveUupSong.stop();
+    mean2meSong.stop();
+    mooncruiseSong.stop();
+    nobrakesSong.stop();
+    wcwSong.pause();
+    wcwSong.play();
+}
+let stop = () => {
+    bonusSong.stop() && byTheWaySong.stop() && disillusionedSong.stop() && giveUupSong.stop() && mean2meSong.stop() && mooncruiseSong.stop() && nobrakesSong.stop() && wcwSong.stop();
+}
+let pause = () => {
+    bonusSong.pause() && byTheWaySong.pause() && disillusionedSong.pause() && giveUupSong.pause() && mean2meSong.pause() && mooncruiseSong.pause() && nobrakesSong.pause() && wcwSong.pause();
+}
 
 //   sound.play();
 
 const Music = () => (
     <div className='music'>
         <div className='space' />
-        <div className='songs'>
-            <div className='song'>
+        <div className='table'>
+            <div className='images'>
                 <img className='songImage' src={bonusPic} alt='fireSpot' />
-                <p>Bonus </p>
-                <p>Play</p>
-                <p>Pause</p>
-                <p>Stop</p>
-            </div>
-            <div className='song'>
                 <img className='songImage' src={noBrakesPic} alt='fireSpot' />
-                <p>n o b r a k e s </p>
-                <p>Play</p>
-                <p>Pause</p>
-                <p>Stop</p>
-            </div>
-            <div className='song'>
                 <img className='songImage' src={byTheWayPic} alt='fireSpot' />
-                <p>by the wayy</p>
-                <p>Play</p>
-                <p>Pause</p>
-                <p>Stop</p>
-            </div>
-            <div className='song'>
                 <img className='songImage' src={moonPic} alt='fireSpot' />
-                <p>moon'n & cruis'n</p>
-                <p>Play</p>
-                <p>Pause</p>
-                <p>Stop</p>
-            </div>
-            <div className='song'>
                 <img className='songImage' src={disillusionedPic} alt='fireSpot' />
-                <p>d i s i l l u s i o n e d</p>
-                <p>Play</p>
-                <p>Pause</p>
-                <p>Stop</p>
-            </div>
-            <div className='song'>
                 <img className='songImage' src={meanPic} alt='fireSpot' />
-                <p>StayLoose - mean 2 me remix</p>
-                <p>Play</p>
-                <p>Pause</p>
-                <p>Stop</p>
-            </div>
-            <div className='song'>
                 <img className='songImage' src={giveUupPic} alt='fireSpot' />
-                <p>give you up</p>
-                <p>Play</p>
-                <p>Pause</p>
-                <p>Stop</p>
-            </div>
-            <div className='song'>
                 <img className='songImage' src={wcwPic} alt='fireSpot' />
+            </div>
+            <div className='names'>
+                <p>Bonus </p>
                 <p>n o b r a k e s </p>
-                <p>Play</p>
-                <p>Pause</p>
-                <p>Stop</p>
+                <p>by the wayy</p>
+                <p>moon'n & cruis'n</p>
+                <p>d i s i l l u s i o n e d</p>
+                <p>StayLoose - mean 2 me remix</p>
+                <p>give you up</p>
+                <p>Your WCW never saved your number </p>
+            </div>
+            <div className='play'>
+                <p onClick={bonus}>Play </p>
+                <p onClick={nobrakes}>Play </p>
+                <p onClick={byTheWay}>Play </p>
+                <p onClick={mooncruise}>Play </p>
+                <p onClick={disillusioned}>Play </p>
+                <p onClick={mean2me}>Play </p>
+                <p onClick={giveUup}>Play </p>
+                <p onClick={wcw}>Play </p>
+            </div>
+            <div className='pause'>
+                <p onClick={pause}>Pause </p>
+                <p onClick={pause}>Pause </p>
+                <p onClick={pause}>Pause </p>
+                <p onClick={pause}>Pause </p>
+                <p onClick={pause}>Pause </p>
+                <p onClick={pause}>Pause </p>
+                <p onClick={pause}>Pause </p>
+                <p onClick={pause}>Pause </p>
+            </div>
+            <div className='stop'>
+                <p onClick={stop}>Stop </p>
+                <p onClick={stop}>Stop </p>
+                <p onClick={stop}>Stop </p>
+                <p onClick={stop}>Stop </p>
+                <p onClick={stop}>Stop </p>
+                <p onClick={stop}>Stop </p>
+                <p onClick={stop}>Stop </p>
+                <p onClick={stop}>Stop </p>
             </div>
         </div>
-
         <div className='foot'>
             <Link to='/'>Home</Link>
         </div>
