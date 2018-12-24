f086930998a82d1d531b028210c9eaf26e53c9b7
evapodaca87 <evapodaca87@gmail.com>
2018-12-23 21:02:53 -0700

    added images

--- src/Component/Music.js
+++ src/Component/Music.js
@@ -1,19 +1,81 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-const {Howl, Howler} = require('howler');
+import bonusPic from '../images/bonus.png';
+import byTheWayPic from '../images/byTheWay.png';
+import disillusionedPic from '../images/disillusioned.png';
+import giveUupPic from '../images/giveUup.png';
+import meanPic from '../images/mean.png';
+import moonPic from '../images/moon.png';
+import noBrakesPic from '../images/noBrakes.png';
+import wcwPic from '../images/wcw.png';
+const { Howl, Howler } = require('howler');
 
 var sound = new Howl({
-    src: ['/Music/bonus.mp3']
-  });
-  
+    src: [ '/Music/bonus.mp3' ]
+});
+
 //   sound.play();
 
 const Music = () => (
     <div className='music'>
         <div className='space' />
         <div className='songs'>
-            <h2>Music Page</h2>
-            <p>Bonus <img src="/images.randomFern.png"></img></p>
+            <div className='song'>
+                <img className='songImage' src={bonusPic} alt='fireSpot' />
+                <p>Bonus </p>
+                <p>Play</p>
+                <p>Pause</p>
+                <p>Stop</p>
+            </div>
+            <div className='song'>
+                <img className='songImage' src={noBrakesPic} alt='fireSpot' />
+                <p>n o b r a k e s </p>
+                <p>Play</p>
+                <p>Pause</p>
+                <p>Stop</p>
+            </div>
+            <div className='song'>
+                <img className='songImage' src={byTheWayPic} alt='fireSpot' />
+                <p>by the wayy</p>
+                <p>Play</p>
+                <p>Pause</p>
+                <p>Stop</p>
+            </div>
+            <div className='song'>
+                <img className='songImage' src={moonPic} alt='fireSpot' />
+                <p>moon'n & cruis'n</p>
+                <p>Play</p>
+                <p>Pause</p>
+                <p>Stop</p>
+            </div>
+            <div className='song'>
+                <img className='songImage' src={disillusionedPic} alt='fireSpot' />
+                <p>d i s i l l u s i o n e d</p>
+                <p>Play</p>
+                <p>Pause</p>
+                <p>Stop</p>
+            </div>
+            <div className='song'>
+                <img className='songImage' src={meanPic} alt='fireSpot' />
+                <p>StayLoose - mean 2 me remix</p>
+                <p>Play</p>
+                <p>Pause</p>
+                <p>Stop</p>
+            </div>
+            <div className='song'>
+                <img className='songImage' src={giveUupPic} alt='fireSpot' />
+                <p>give you up</p>
+                <p>Play</p>
+                <p>Pause</p>
+                <p>Stop</p>
+            </div>
+            <div className='song'>
+                <img className='songImage' src={wcwPic} alt='fireSpot' />
+                <p>n o b r a k e s </p>
+                <p>Play</p>
+                <p>Pause</p>
+                <p>Stop</p>
+            </div>
         </div>
 
         <div className='foot'>
